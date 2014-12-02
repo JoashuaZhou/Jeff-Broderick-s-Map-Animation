23a18564450e4257d629ca37dcb20e7fc679c4b2
a226035102 <comeonjiji@gmail.com>
2014-12-02 15:27:22 +0800

    Initial Commit

---
+++ Jeff Broderick’s Map Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jeff Broderick’s Map Animation
+//
+//  Created by Joshua Zhou on 14/12/2.
+//  Copyright (c) 2014年 Joshua Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jeff Broderick’s Map Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jeff Broderick’s Map Animation
+//
+//  Created by Joshua Zhou on 14/12/2.
+//  Copyright (c) 2014年 Joshua Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
